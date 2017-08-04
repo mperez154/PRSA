c9871c9d67cf585e1c9f8c85957251044eeecf4c
Marco Perez <mperez@parkingreservationsoftware.com>
2017-08-04 15:08:18 -0400

    Updated Strings class to use shorter names

--- Automation/Data/strngs.cs
+++ Automation/Data/strngs.cs
@@ -7,19 +7,19 @@
         private static string loyaltyLoginPage = "http://node1-qa/smart.luis/Loyalty/Login?uniqueid=50027bc7-3591-4530-9284-224500614542";
         private static string adminLoginPage = "http://node1-qa/Smart.Luis/Admin/Account/Login?returnUrl=/smart.luis/admin/home";
 
-        public static string getCreateReservationPage()
+        public static string getReservationPage()
         {
             return createReservationPage;
         }
-        public static string getFindReservationPage()
+        public static string getFindPage()
         {
             return findReservationPage;
         }
-        public static string getLoyaltyLoginPage()
+        public static string getLoyaltyPage()
         {
             return loyaltyLoginPage;
         }
-        public static string getAdminLoginPage()
+        public static string getAdminPage()
         {
             return adminLoginPage;
         }
--- Automation/TestCases/FindReserveTest.cs
+++ Automation/TestCases/FindReserveTest.cs
@@ -8,20 +8,19 @@ namespace Automation.TestCases
     class FindReserveTest: BaseTest
     {
         [Test, Property("Priority", 1)]
-        public void MyFirstTest()
+        public void validateURL()
         {
             //Should pass
-            FindAndReserve.GetSite(driver, strngs.getCreateReservationPage());
-            Assert.AreEqual("http://node1-qa/smart.luis/Reservations/Reserve/?uniqueid=50027BC7-3591-4530-9284-224500614542", driver.Url);
-            Assert.AreEqual("WallyPark Reservations", driver.Title);
+            FindAndReserve.GetSite(driver, strngs.getReservationPage());
+            Assert.AreEqual("http://node1-qa/smart.luis/Reservations/Reserve/?uniqueid=50027BC7-3591-4530-9284-224500614542", driver.Url);          
         }
 
         [Test, Property("Priority", 2)]
-        public void MySecondTest()
+        public void validateTitle()
         {
             //Should Fail
-            FindAndReserve.GetSite(driver, strngs.getCreateReservationPage());
-            Assert.AreEqual("www.google.com", driver.Url);
+            FindAndReserve.GetSite(driver, strngs.getReservationPage());
+            Assert.AreEqual("WallyPark Reservation Fail", driver.Title);
         }
     }
 }
